241e4e6837354073df57cf36a9f42ca432ce2596
Chau Duong <chau.duong@leflair.vn>
2019-03-16 17:25:18 +0700

    feat: add examples with boostrap and sass

---
+++ examples/with-boostrap-sass/README.md
@@ -0,0 +1,9 @@
+# With styled-jsx plugins
+
+## How to use
+
+Install it and run:
+
+yarn
+yarn dev
+```
---
+++ examples/with-boostrap-sass/pages/index.jsx
@@ -0,0 +1,8 @@
+export default () => (
+  <div>
+    <div className='hero'>
+      <h1 className='title mr-3'>Next.js + boostrap + sass</h1>
+      <button className="btn btn-primary">test boostrap</button>
+    </div>
+  </div>
+)
